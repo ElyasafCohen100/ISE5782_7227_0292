3f35021b543012af890670052f51761f223b69f9
ElyasafCohen100 <elyasaf93@gmail.com>
2022-05-12 14:03:40 +0300

    Targil 5 has been done! :)

--- unitTests/renderer/RenderTests.java
+++ unitTests/renderer/RenderTests.java
@@ -23,14 +23,12 @@ public class RenderTests {
 		Scene scene = new Scene.SceneBuilder("Test scene")
 				.setAmbientLight(new AmbientLight(new Color(255, 191, 191),new Double3(1,1,1)))
 				.setBackground(new Color(75, 127, 90))
-				.build();
-
-		scene.getGeometries().add(new Sphere(new Point(0, 0, -100), 50),
+				.setGeometries(new Geometries(new Sphere(new Point(0, 0, -100), 50),
 				new Triangle(new Point(-100, 0, -100), new Point(0, 100, -100), new Point(-100, 100, -100)), // up
 				// left
 				new Triangle(new Point(-100, 0, -100), new Point(0, -100, -100), new Point(-100, -100, -100)), // down
 				// left
-				new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100))); // down
+				new Triangle(new Point(100, 0, -100), new Point(0, -100, -100), new Point(100, -100, -100)))).build(); // down
 		// right
 		Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0))
 				.setVPDistance(100)
@@ -39,23 +37,26 @@ public class RenderTests {
 				.setRayTracer(new RayTracerBasic(scene));
 
 		camera.renderImage();
-		camera.printGrid(100, new Color(java.awt.Color.YELLOW));
+		camera.printGrid(50, new Color(java.awt.Color.YELLOW));
 		camera.writeToImage();
 	}
 	/**
+	 * Produce a scene with basic 3D model and render it into a png image with a
+	 * grid
+	 */	}
+	/**
 	 * Test for XML based scene - for bonus
 	 */
-	@Test
-	public void basicRenderXml() {
-		Scene.SceneBuilder scene = new Scene.SceneBuilder("XML Test scene");
-
-		Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
-				.setVPDistance(100) //
-				.setVPSize(500, 500)
-				.setImageWriter(new ImageWriter("xml render test", 1000, 1000))
-				.setRayTracer(new RayTracerBasic(scene.build()));
-		camera.renderImage();
-		camera.printGrid(100, new Color(java.awt.Color.YELLOW));
-		camera.writeToImage();
-	}
-}
+	//@Test
+//	public void basicRenderXml() {
+//		Scene.SceneBuilder scene = new Scene.SceneBuilder("XML Test scene");
+//
+//		Camera camera = new Camera(Point.ZERO, new Vector(0, 0, -1), new Vector(0, 1, 0)) //
+//				.setVPDistance(100) //
+//				.setVPSize(500, 500)
+//				.setImageWriter(new ImageWriter("xml render test", 1000, 1000))
+//				.setRayTracer(new RayTracerBasic(scene.build()));
+//		camera.renderImage();
+//		camera.printGrid(100, new Color(java.awt.Color.YELLOW));
+//		camera.writeToImage();
+//	}
